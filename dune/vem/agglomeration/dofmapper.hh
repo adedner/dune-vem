1adfc3d526a0688610413210cd827822a5244085
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2015-10-12 13:06:38 +0200

    build complete list of subAgglomerates

--- dune/vem/agglomeration/dofmapper.hh
+++ dune/vem/agglomeration/dofmapper.hh
@@ -18,20 +18,21 @@ namespace Dune
     // AgglomerationDofMapper
     // ----------------------
 
-    template< class GridPart >
+    template< class GridPart, class Layout >
     class AgglomerationDofMapper
     {
       typedef AgglomerationDofMapper< GridPart > ThisType;
 
-      typedef typename GridPart::IndexSetType IndexSetType;
-
     public:
       typedef GridPart GridPartType;
 
-      AgglomerationDofMapper ( const GridPartType &gridPart, std::vector< std::size_t > agglomerateIndex );
+      static const int dimension = GridPartType::dimension;
+
+      AgglomerationDofMapper ( const GridPartType &gridPart, std::vector< std::size_t > agglomerateIndex, Layout layout = Layout() );
 
     private:
       const GridPartType &gridPart_;
+      Layout layout_;
       std::vector< std::size_t > agglomerateIndices_;
       std::vector< std::vector< std::size_t > > agglomerateConnectivity_;
     };
@@ -41,27 +42,50 @@ namespace Dune
     // Implementation of AgglomerationDofMapper
     // ---------------------------------------
 
-    template< class GridPart >
-    inline AgglomerationDofMapper::AgglomerationDofMapper ( const GridPartType &gridPart, std::vector< std::size_t > agglomerateIndices )
-      : gridPart_( gridPart ), agglomerateIndices_( std::move( agglomerateIndices ) )
+    template< class GridPart, class Layout >
+    inline AgglomerationDofMapper< GridPart, Layout >
+      ::AgglomerationDofMapper ( const GridPartType &gridPart, std::vector< std::size_t > agglomerateIndices, Layout layout )
+      : gridPart_( gridPart ), layout_( layout ), agglomerateIndices_( std::move( agglomerateIndices ) )
     {
-      const IndexSetType &indexSet = gridPart_.indexSet();
+      typedef typename GridPart::IndexSetType::IndexType IndexType;
 
-      // find faces of agglomerates
+      const typename GridPart::IndexSetType &indexSet = gridPart_.indexSet();
 
-      std::vector< typename IndexSetType::IndexType > agFaces;
+      std::vector< std::vector< IndexType > subAgglomerates( GlobalGeometryTypeIndex::size( dimension-1 ) );
+
+      // find subagglomerates
+
       for( const auto element : elements( gridPart, Partitions::interiorBorder ) )
       {
         const std::size_t agIndex = agglomerateIndices_[ indexSet.index( element ) ];
+        const auto &refElement = ReferenceElements< typename GridPart::ctype, dimension >::general( element.type() );
         for( const auto intersection : intersections( gridPart, element ) )
         {
           assert( intersection.conforming() );
           if( !intersection.neighbor() || (agglomerateIndices_[ indexSet.index( intersection.outside() ) ] != agIndex) )
-            agFaces.push_back( indexSet.index( element.template subEntity< 1 >( intersection.indexInInside() ) ) );
+          {
+            const int face = intersection.indexInInside();
+            for( int codim = 1; codim <= dimension; ++codim )
+            {
+              const int numSubEntities = refElement.size( face, 1, codim );
+              for( int i = 0; i < numSubEntities; ++i )
+              {
+                const int k = refElement.subentity( face, 1, i, codim );
+                const std::size_t typeIndex = GlobalGeometryTypeIndex::index( refElement.type( k, codim ) );
+                subAgglomerates[ typeIndex ].push_back( indexSet.subIndex( element, k, codim ) );
+              }
+            }
+          }
         }
       }
-      std::sort( agFaces.begin(), agFaces.end() );
-      agFaces.erase( std::unique( agFaces.begin(), agFaces.end() ), agFaces.end() );
+
+      // make subagglomerates unique
+
+      for( auto &list : subAgglomerates )
+      {
+        std::sort( list.begin(), list.end() );
+        list.erase( std::unique( list.begin(), list.end() ), list.end() );
+      }
     }
 
   } // namespace Vem
