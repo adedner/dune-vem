f86c2738519a593a3c69439a362d5196bf1c8de9
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2015-12-04 16:26:41 +0100

    expand tabs

--- src/poisson-test/dune-poisson-agglo-DG.cc
+++ src/poisson-test/dune-poisson-agglo-DG.cc
@@ -51,133 +51,133 @@ typedef Dune::UGGrid< 2 > Grid;
 int main ( int argc, char **argv )
 try
 {
-	Dune::Fem::MPIManager::initialize( argc, argv );
+  Dune::Fem::MPIManager::initialize( argc, argv );
 
-	// append overloaded parameters from the command line
-	Dune::Fem::Parameter::append( argc, argv );
+  // append overloaded parameters from the command line
+  Dune::Fem::Parameter::append( argc, argv );
 
-	// append possible given parameter files
-	for( int i = 1; i < argc; ++i )
-		Dune::Fem::Parameter::append( argv[ i ] );
+  // append possible given parameter files
+  for( int i = 1; i < argc; ++i )
+    Dune::Fem::Parameter::append( argv[ i ] );
 
-	// append default parameter file
-	Dune::Fem::Parameter::append( "/home/gcd3/codes/dune-vem/data/parameter" );
+  // append default parameter file
+  Dune::Fem::Parameter::append( "/home/gcd3/codes/dune-vem/data/parameter" );
 
-	const std::string gridkey = Dune::Fem::IOInterface::defaultGridKey( Grid::dimension );
-	const std::string gridfile = Dune::Fem::Parameter::getValue< std::string >( gridkey );
+  const std::string gridkey = Dune::Fem::IOInterface::defaultGridKey( Grid::dimension );
+  const std::string gridfile = Dune::Fem::Parameter::getValue< std::string >( gridkey );
 
-	// the method rank and size from MPIManager are static
-	if( Dune::Fem::MPIManager::rank() == 0 )
-		std::cout << "Loading macro grid: " << gridfile << std::endl;
+  // the method rank and size from MPIManager are static
+  if( Dune::Fem::MPIManager::rank() == 0 )
+    std::cout << "Loading macro grid: " << gridfile << std::endl;
 
-	//  const auto sectionMap = Gmsh::readFile( argv[ 1 ] );
-	const auto sectionMap = Gmsh::readFile (gridfile);
-	const auto nodes = Gmsh::parseNodes( sectionMap );
-	const auto elements = Gmsh::parseElements( sectionMap );
+  //  const auto sectionMap = Gmsh::readFile( argv[ 1 ] );
+  const auto sectionMap = Gmsh::readFile (gridfile);
+  const auto nodes = Gmsh::parseNodes( sectionMap );
+  const auto elements = Gmsh::parseElements( sectionMap );
 
-	const auto entities = Gmsh::duneEntities( elements, Grid::dimension );
-	const std::vector< std::size_t > vertices = Gmsh::vertices( entities );
-
-
-
-
-	Dune::GridFactory< Grid > factory;
-	Gmsh::insertVertices( factory, vertices, nodes );
-	Gmsh::insertElements( factory, entities, vertices );
-
-	std::unique_ptr< Grid > grid( factory.createGrid() );
-
-	std::vector< std::size_t > elementIds = Gmsh::elements( grid->leafGridView(), factory, entities ); // contains the element ids of the original grid
-	std::vector< int > agglomerateIndices = Gmsh::tags( elements, elementIds, 3 ); // contains the ids of individual element in each agglomerated polygon
-	std::transform( agglomerateIndices.begin(), agglomerateIndices.end(), agglomerateIndices.begin(),
-			[] ( int i ) { return i-1; } ); // This will give you correct number of agglomerations!
-	// create grid part and agglomeration
-
-	typedef Dune::Fem::AdaptiveLeafGridPart< Grid > GridPart;
-	GridPart gridPart( *grid );
-
-	Dune::Vem::Agglomeration< GridPart > agglomeration( gridPart, agglomerateIndices );
-
-
-	std::cout << " agglomeration.size() " << agglomeration.size() << std::endl;
-	// define a function space type
-	typedef Dune::Fem::FunctionSpace< GridPart::ctype, double, GridPart::dimension, 1 > FunctionSpaceType;
+  const auto entities = Gmsh::duneEntities( elements, Grid::dimension );
+  const std::vector< std::size_t > vertices = Gmsh::vertices( entities );
 
 
 
-	// * * * Poisson problem solution * * * //
 
-	typedef DiffusionModel< FunctionSpaceType, GridPart > ModelType;
-	typedef typename ModelType::ProblemType ProblemType ;
-	ProblemType* problemPtr = 0 ;
-	const std::string problemNames [] = { "cos", "sphere", "sin", "corner", "curvedridges" };
-	const int problemNumber = Dune::Fem::Parameter::getEnum("poisson.problem", problemNames, 0 );
+  Dune::GridFactory< Grid > factory;
+  Gmsh::insertVertices( factory, vertices, nodes );
+  Gmsh::insertElements( factory, entities, vertices );
 
-	switch ( problemNumber )
-	{
-	case 0:
-		problemPtr = new CosinusProduct< FunctionSpaceType > ();
-		break ;
-	case 1:
-		problemPtr = new SphereProblem< FunctionSpaceType > ();
-		break ;
-	case 2:
-		problemPtr = new SinusProduct< FunctionSpaceType > ();
-		break ;
-	case 3:
-		problemPtr = new ReentrantCorner< FunctionSpaceType > ();
-		break ;
-	case 4:
-		problemPtr = new CurvedRidges< FunctionSpaceType > ();
-		break ;
-	default:
-		problemPtr = new CosinusProduct< FunctionSpaceType > ();
-	}
-	assert( problemPtr );
-	ProblemType& problem = *problemPtr ;
+  std::unique_ptr< Grid > grid( factory.createGrid() );
 
-	// implicit model for left hand side
-	ModelType implicitModel( problem, gridPart );
+  std::vector< std::size_t > elementIds = Gmsh::elements( grid->leafGridView(), factory, entities ); // contains the element ids of the original grid
+  std::vector< int > agglomerateIndices = Gmsh::tags( elements, elementIds, 3 ); // contains the ids of individual element in each agglomerated polygon
+  std::transform( agglomerateIndices.begin(), agglomerateIndices.end(), agglomerateIndices.begin(),
+      [] ( int i ) { return i-1; } ); // This will give you correct number of agglomerations!
+  // create grid part and agglomeration
 
-	std::cout << "going in femscheme" << std::endl;
-	typedef FemScheme< ModelType > SchemeType;
-	SchemeType scheme( gridPart, implicitModel, agglomeration );
+  typedef Dune::Fem::AdaptiveLeafGridPart< Grid > GridPart;
+  GridPart gridPart( *grid );
 
-	typedef Dune::Fem::GridFunctionAdapter< ProblemType, GridPart > GridExactSolutionType;
-	GridExactSolutionType gridExactSolution("exact solution", problem, gridPart, 5 );
+  Dune::Vem::Agglomeration< GridPart > agglomeration( gridPart, agglomerateIndices );
 
-	//! input/output tuple and setup datawritter
-	typedef Dune::tuple<const typename SchemeType::DiscreteFunctionType *, GridExactSolutionType * > IOTupleType;
-	typedef Dune::Fem::DataOutput< Grid, IOTupleType > DataOutputType;
-	IOTupleType ioTuple( &(scheme.solution()), &gridExactSolution) ; // tuple with pointers
+
+  std::cout << " agglomeration.size() " << agglomeration.size() << std::endl;
+  // define a function space type
+  typedef Dune::Fem::FunctionSpace< GridPart::ctype, double, GridPart::dimension, 1 > FunctionSpaceType;
 
 
 
-	// setup the right hand side
-	scheme.prepare();
-	// solve once (assemble matrix)
-	scheme.solve(true);
+  // * * * Poisson problem solution * * * //
 
-	// VTK output
+  typedef DiffusionModel< FunctionSpaceType, GridPart > ModelType;
+  typedef typename ModelType::ProblemType ProblemType ;
+  ProblemType* problemPtr = 0 ;
+  const std::string problemNames [] = { "cos", "sphere", "sin", "corner", "curvedridges" };
+  const int problemNumber = Dune::Fem::Parameter::getEnum("poisson.problem", problemNames, 0 );
 
-	Dune::Fem::VTKIO< GridPart > vtkIO( gridPart, Dune::VTK::nonconforming );
-	vtkIO.addVertexData( scheme.solution() );
-	vtkIO.write( "test-poisson", Dune::VTK::ascii );
+  switch ( problemNumber )
+  {
+  case 0:
+    problemPtr = new CosinusProduct< FunctionSpaceType > ();
+    break ;
+  case 1:
+    problemPtr = new SphereProblem< FunctionSpaceType > ();
+    break ;
+  case 2:
+    problemPtr = new SinusProduct< FunctionSpaceType > ();
+    break ;
+  case 3:
+    problemPtr = new ReentrantCorner< FunctionSpaceType > ();
+    break ;
+  case 4:
+    problemPtr = new CurvedRidges< FunctionSpaceType > ();
+    break ;
+  default:
+    problemPtr = new CosinusProduct< FunctionSpaceType > ();
+  }
+  assert( problemPtr );
+  ProblemType& problem = *problemPtr ;
+
+  // implicit model for left hand side
+  ModelType implicitModel( problem, gridPart );
+
+  std::cout << "going in femscheme" << std::endl;
+  typedef FemScheme< ModelType > SchemeType;
+  SchemeType scheme( gridPart, implicitModel, agglomeration );
+
+  typedef Dune::Fem::GridFunctionAdapter< ProblemType, GridPart > GridExactSolutionType;
+  GridExactSolutionType gridExactSolution("exact solution", problem, gridPart, 5 );
+
+  //! input/output tuple and setup datawritter
+  typedef Dune::tuple<const typename SchemeType::DiscreteFunctionType *, GridExactSolutionType * > IOTupleType;
+  typedef Dune::Fem::DataOutput< Grid, IOTupleType > DataOutputType;
+  IOTupleType ioTuple( &(scheme.solution()), &gridExactSolution) ; // tuple with pointers
 
 
-	// calculate standard error
-	// select norm for error computation
-	typedef Dune::Fem::L2Norm< GridPart > NormType;
-	NormType norm( gridPart );
-	std::cout << norm.distance( gridExactSolution, scheme.solution() ) << std::endl;
-	std::cout << "Finished!" << std::endl;
-	return 0;
+
+  // setup the right hand side
+  scheme.prepare();
+  // solve once (assemble matrix)
+  scheme.solve(true);
+
+  // VTK output
+
+  Dune::Fem::VTKIO< GridPart > vtkIO( gridPart, Dune::VTK::nonconforming );
+  vtkIO.addVertexData( scheme.solution() );
+  vtkIO.write( "test-poisson", Dune::VTK::ascii );
+
+
+  // calculate standard error
+  // select norm for error computation
+  typedef Dune::Fem::L2Norm< GridPart > NormType;
+  NormType norm( gridPart );
+  std::cout << norm.distance( gridExactSolution, scheme.solution() ) << std::endl;
+  std::cout << "Finished!" << std::endl;
+  return 0;
 
 
 
 }
 catch( const Dune::Exception &e )
 {
-	std::cout << e << std::endl;
-	return 1;
+  std::cout << e << std::endl;
+  return 1;
 }
