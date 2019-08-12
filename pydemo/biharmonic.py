7a7a1c27581bbe0bfabba4fa1dc46aa9eff08a17
Alice Hodson <alicehodson@live.co.uk>
2019-08-12 14:52:57 +0100

    use spsolve for biharmonic problem

--- pydemo/biharmonic.py
+++ pydemo/biharmonic.py
@@ -13,6 +13,8 @@ from dune.fem.plotting import plotPointData as plot
 from dune.fem.function import integrate
 from dune.fem import parameter
 from dune.vem import voronoiCells
+from dune.fem.operator import linear as linearOperator
+from scipy.sparse.linalg import spsolve
 
 from ufl import *
 import dune.ufl
@@ -34,8 +36,8 @@ methods = [ ### "[space,scheme,spaceKwrags]"
 parameters = {"newton.linear.tolerance": 1e-12,
               "newton.linear.preconditioning.method": "jacobi",
               "penalty": 40,  # for the bbdg scheme
-              "newton.linear.verbose": False,
-              "newton.verbose": False
+              "newton.linear.verbose": True,
+              "newton.verbose": True
               }
 
 # <markdowncell>
@@ -50,14 +52,15 @@ exact = as_vector( [sin(2*pi*x[0])**2*sin(2*pi*x[1])**2] )
 laplace = lambda w: div(grad(w))
 u = TrialFunction(uflSpace)
 v = TestFunction(uflSpace)
-a = ( inner(laplace(u[0]),laplace(v[0])) ) * dx
+a = ( inner(laplace(u[0]),laplace(v[0])) ) * dx + inner(grad(u),grad(v)) * dx + inner(u,v) * dx
 
 # finally the right hand side and the boundary conditions
-b = ( laplace(laplace(exact[0])) ) * v[0] * dx
-dbc = [dune.ufl.DirichletBC(uflSpace, [0], i+1) for i in range(4)]
-biLaplaceCoeff = 10000
-diffCoeff = 0
-massCoeff = 0
+b = laplace(laplace(exact[0]))*v[0] * dx - laplace(exact[0])*v[0] * dx + exact[0]*v[0] * dx
+# dbc = [dune.ufl.DirichletBC(uflSpace, [0], i+1) for i in range(4)]
+dbc = []
+biLaplaceCoeff = 1
+diffCoeff = 1
+massCoeff = 1
 
 # <markdowncell>
 # Now we define a grid build up of voronoi cells around $50$ random points
@@ -68,7 +71,7 @@ massCoeff = 0
 # <codecell>
 def compute(grid, space, schemeName):
     # solve the pde
-    df = space.interpolate(exact,name="solution")
+    df = space.interpolate([0],name="solution")
     # df.plot(level=3)
     info = {"linear_iterations":1}
     scheme = create.scheme(schemeName, [a==b, *dbc], space, solver="cg",
@@ -76,7 +79,12 @@ def compute(grid, space, schemeName):
                         gradStabilization=diffCoeff,
                         massStabilization=massCoeff,
                       parameters=parameters)
-    info = scheme.solve(target=df)
+    # info = scheme.solve(target=df)
+    jacobian = linearOperator(scheme)
+    rhs = space.interpolate([0],name="rhs")
+    scheme(df,rhs)
+    rhs.as_numpy[:] *= -1
+    df.as_numpy[:] = spsolve(jacobian.as_numpy, rhs.as_numpy[:])
     edf = exact-df
     err = [inner(edf,edf),
            inner(grad(edf),grad(edf)),
@@ -95,7 +103,7 @@ for level in range(maxLevel):
     constructor = cartesianDomain([-0.5,-0.5],[1,1],[40*2**level,40*2**level])
     polyGrid = create.grid("polygrid", constructor, cubes=False )
     for i,m in enumerate(methods):
-        space = create.space(m[0], polyGrid, order=order, dimRange=1, storage="istl", **m[2])
+        space = create.space(m[0], polyGrid, order=order, dimRange=1, storage="fem", **m[2])
         dfs,errors,info = compute(polyGrid, space, m[1])
         print("method:(",m[0],m[2],")",
               "Size: ",space.size, "L^2: ", errors[0], "H^1: ", errors[1],
