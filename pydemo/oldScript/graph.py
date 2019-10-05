022c77fcd376245124939b6da0d2aeced63a4fa9
dedner <a.s.dedner@warwick.ac.uk>
2019-10-05 19:59:02 +0100

    remove pdf files - should be put into paper archive

--- pydemo/oldScript/graph.py
+++
@@ -1,9 +0,0 @@
-import metis
-def partition(grid,nparts,**opts):
-    graph = []
-    indexSet = grid.indexSet
-    for e in grid.elements:
-        edges = (indexSet.index(i.outside) for i in grid.intersections(e) if i.outside)
-        graph += [ tuple(edges) ]
-    (edgecuts, parts) = metis.part_graph(graph, nparts=nparts,recursive=False,**opts)
-    return parts
