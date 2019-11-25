7991bc74cfe9d5d1fef1fa2d54c0dfc8c0186714
Alice Hodson <alicehodson@live.co.uk>
2019-11-25 14:26:35 +0000

    updated test script for least squares

--- dune/vem/misc/leastSquares.hh
+++ dune/vem/misc/leastSquares.hh
@@ -35,14 +35,12 @@ namespace Dune {
 
                     Size systemMatrixDim = systemMatrixInv_.rows();
 
-                    // since systemMatrix sqaure, rows = cols
+                    // since systemMatrix square, rows = cols
                     Vector systemMultiply(systemMatrixDim, 0);
 
-
-                    for ( Size i = 0; i < systemMatrixDim; ++i) {
+                    for ( Size i = 0; i < systemMatrixDim; ++i)
                         for (Size j = 0; j < systemMatrixDim; ++j)
                             systemMultiply[i] += systemMatrixInv_[i][j] * systemVector[j];
-                    }
 
                     for ( Size i = 0; i < solution.size(); ++i)
                         solution[i] = systemMultiply[i];
@@ -62,7 +60,6 @@ namespace Dune {
                     }
                 }
 
-//                template< class Matrix >
                 void printMatrix(const Matrix &A)
                 {
                     for(unsigned int i = 0; i < A.rows(); ++i) {
@@ -85,143 +82,45 @@ namespace Dune {
                 const Matrix &llsMatrix_;
                 const Matrix &constraintMatrix_;
                 const Matrix systemMatrixInv_;
-//                const Size LLSMatrixNumRows_ = llsMatrix_.rows();
-//                const Size LLSMatrixNumCols_ = llsMatrix_.cols();
-//                const Size constraintMatrixNumCols_ = constraintMatrix_.cols();
-//                const Size constraintMatrixNumRows_ = constraintMatrix_.rows();
-
-                Size llsMatrixNumRows()
-                {
-                    return llsMatrix_.rows();
-                }
-
-                Size llsMatrixNumCols()
-                {
-                    return llsMatrix_.cols();
-                }
-
-                Size constraintMatrixNumRows()
-                {
-                    return constraintMatrix_.rows();
-                }
-
-                Size constraintMatrixNumCols()
-                {
-                    return constraintMatrix_.cols();
-                }
 
                 Matrix matrixSetUp()
                 {
                     // construct the matrix [2A^T*A C^T ; C 0] needed for least squares solution
                     Matrix systemMatrix;
 
-                    const Size LLSMatrixNumRows_ = llsMatrixNumRows();
-                    const Size LLSMatrixNumCols_ = llsMatrixNumCols();
-                    const Size constraintMatrixNumCols_ = constraintMatrixNumCols();
-                    const Size constraintMatrixNumRows_ = constraintMatrixNumRows();
-
-//                    std::cout << "matrix llsMatrix has columns: "<< LLSMatrixNumCols_ << std::endl;
-//                    std::cout << "matrix llsMatrix has rows: "<< LLSMatrixNumRows_ << std::endl;
-//
-//                    std::cout << "matrix constraint has columns: "<< constraintMatrixNumCols_ << std::endl;
-//                    std::cout << "matrix constraint has rows: "<< constraintMatrixNumRows_ << std::endl;
-
-//                    std::cout << "matrix llsMatrix has columns: "<< llsMatrixNumCols() << std::endl;
-//                    std::cout << "matrix llsMatrix has rows: "<< llsMatrixNumRows() << std::endl;
-//
-//                    std::cout << "matrix constraint has columns: "<< constraintMatrixNumCols() << std::endl;
-//                    std::cout << "matrix constraint has rows: "<< constraintMatrixNumRows() << std::endl;
-
                     // check dimensions compatible
+                    assert( llsMatrix_.cols() == constraintMatrix_.cols() );
+                    assert( ( llsMatrix_.rows() + constraintMatrix_.rows() >= constraintMatrix_.cols() )
+                    && constraintMatrix_.rows() <= constraintMatrix_.cols() );
 
-                    assert( llsMatrixNumCols() == constraintMatrixNumCols() );
-                    assert( ( llsMatrixNumRows() + constraintMatrixNumRows() >= constraintMatrixNumCols() )
-                    && constraintMatrixNumRows() <= constraintMatrixNumCols() );
-//                    assert( LLSMatrixNumCols_ == constraintMatrixNumCols_ );
-//                    assert( (LLSMatrixNumRows_ + constraintMatrixNumRows_ >= constraintMatrixNumCols_) && (constraintMatrixNumRows_ <= constraintMatrixNumCols_) );
-
-                    systemMatrix.resize( (LLSMatrixNumCols_ + constraintMatrixNumRows_), (LLSMatrixNumCols_ + constraintMatrixNumRows_), 0 );
-
-//                    std::cout << "I reached here 1" << std::endl;
-
-//                    std::cout << systemMatrix.cols() << std::endl;
-//                    std::cout << systemMatrix.rows() << std::endl;
+                    systemMatrix.resize( (llsMatrix_.cols() + constraintMatrix_.rows()), (llsMatrix_.cols() + constraintMatrix_.rows() ), 0 );
 
                     std::cout << "System matrix: " << std::endl;
                     printMatrix(systemMatrix);
-
-//                    std::cout << "I reached here 2" << std::endl;
-
-//                    std::cout << LLSMatrixNumCols_ << std::endl;
 
                     // fill up system matrix
                     for ( Size i = 0; i < systemMatrix.rows(); ++i) {
-//                        std::cout << "I reached here 3" << std::endl;
-                        if (i < LLSMatrixNumCols_)
-                        {
-//                            std::cout << "I reached here 4" << std::endl;
+                        if (i < llsMatrix_.cols() ) {
                             for (Size j = 0; j < systemMatrix.cols(); ++j){
-                                if (j < LLSMatrixNumCols_)
-                                {
-//                                    std::cout << "I reached here 5" << std::endl;
-
-                                    for (Size k = 0; k < LLSMatrixNumRows_; ++k)
+                                if (j < llsMatrix_.cols() ) {
+                                    for (Size k = 0; k < llsMatrix_.rows(); ++k)
                                         systemMatrix[ i ][ j ] += 2 * llsMatrix_[k][i] * llsMatrix_[k][j];
                                 }
-                                else
-                                {
-//                                    std::cout << "I reached here 6" << std::endl;
-                                    systemMatrix[ i ][ j ] += constraintMatrix_[ j - LLSMatrixNumCols_ ][ i ];
+                                else {
+                                    systemMatrix[ i ][ j ] += constraintMatrix_[ j - llsMatrix_.cols() ][ i ];
                                 }
                             }
                         }
-
                         else {
-                            for (Size j = 0; j < LLSMatrixNumCols_; ++j) {
-//                                std::cout << "I reached here 7 " << "i: " << i << " " << constraintMatrixNumRows_
-//                                          << std::endl;
-//                                int m = i - constraintMatrixNumRows();
-//
-//
-//                                std::cout << (i - constraintMatrixNumRows_) << std::endl;
-//                                std::cout << i - constraintMatrixNumRows() << std::endl;
-//                                std::cout << m << std::endl;
-
-                                systemMatrix[i][j] += constraintMatrix_[i - constraintMatrixNumRows_][j];
-//                                std::cout << "I reached here 8" << std::endl;
-                            }
+                            for (Size j = 0; j < llsMatrix_.cols(); ++j)
+                                systemMatrix[i][j] += constraintMatrix_[i - constraintMatrix_.rows() ][j];
                         }
-
-//                        std::cout << "I reached here 9" << std::endl;
                     }
-
-//                    for ( Size i = 0; i < LLSMatrixNumCols_; ++i)
-//                        for ( Size j = 0; j < LLSMatrixNumCols_; ++j)
-//                            for ( Size k = 0; k < LLSMatrixNumRows_; ++k)
-//                                systemMatrix[ i ][ j ] += 2*llsMatrix_[ k ][ i ]*llsMatrix_[ k ][ j ];
 
                     std::cout << "System matrix: " << std::endl;
                     printMatrix(systemMatrix);
 
-//                    std::cout << systemMatrix.size() << std::endl;
-
-                    assert(LLSMatrixNumCols_+1 < systemMatrix.size());
-//
-//                    for ( int i = (LLSMatrixNumCols_ + 1 ); i < systemMatrix.size(); ++i)
-//                        for ( int j = 0; j < LLSMatrixNumCols_; ++j)
-//                            systemMatrix[ i ][ j ] += constraintMatrix_[ i ][ j ];
-
-//                    std::cout << "I reached here 3" << std::endl;
-//                    printMatrix(systemMatrix);
-
-//                    for ( Size i = 0; i < LLSMatrixNumCols_; ++i)
-//                        for ( Size j = (LLSMatrixNumCols_ +1); j < systemMatrix.size(); ++j)
-//                            systemMatrix[ i ][ j ] += constraintMatrix_[ j ][ i ];
-//
-//                    printMatrix(systemMatrix);
-
-//                    Vector systemVector = vectorSetUp()
-//                    systemMatrix.solve()
+                    assert( llsMatrix_.cols()+1 < systemMatrix.size());
 
                     systemMatrix.invert();
 
@@ -234,33 +133,19 @@ namespace Dune {
                 template <class Vector>
                 Vector vectorSetUp( const Vector &b, const Vector &d )
                 {
-                    const Size LLSMatrixNumRows_ = llsMatrixNumRows();
-                    const Size LLSMatrixNumCols_ = llsMatrixNumCols();
-                    const Size constraintMatrixNumCols_ = constraintMatrixNumCols();
-                    const Size constraintMatrixNumRows_ = constraintMatrixNumRows();
+                    assert( ( llsMatrix_.rows() == b.size()) && (( llsMatrix_.cols() + d.size())
+                    == (llsMatrix_.cols() + constraintMatrix_.rows() )));
 
-                    assert( (LLSMatrixNumRows_ == b.size()) && ((LLSMatrixNumCols_ + d.size())
-                    == (LLSMatrixNumCols_ + constraintMatrixNumRows_ )));
-
-//                    std::vector< typename Vector::value_t > systemVector( (LLSMatrixNumCols_ + constraintMatrixNumRows_), 0 );
-
-                    Vector systemVector(LLSMatrixNumCols_ + constraintMatrixNumRows_);
-                    Vector y(LLSMatrixNumCols_,0);
+                    Vector systemVector( llsMatrix_.cols() + constraintMatrix_.rows()), y(llsMatrix_.cols(),0);
 
                     // calculate y = 2 * A^T * b
                     llsMatrix_.usmtv(2,b,y);
 
                     for (Size i = 0; i < systemVector.size(); ++i){
-                        if (i < LLSMatrixNumCols_)
-                        {
+                        if (i < llsMatrix_.cols() ) {
                             systemVector[i] = y[i];
-//                            for(Size j = 0; j < LLSMatrixNumRows_; ++j)
-//                            {
-//                                systemVector[i] += 2*llsMatrix_[j][i]*b[j];
-//                            }
                         }
-                        else
-                        {
+                        else {
                             systemVector[i] = d[i - y.size()];
                         }
                     }
--- src/test-leastsquares.cc
+++ src/test-leastsquares.cc
@@ -32,17 +32,12 @@ int main()
     Dune::DynamicVector< double > b(3,0), d(3), x(3,0);
     Dune::DynamicVector< double > exactSoln(3,0);
 
+    const double tol = 1e-10;
+
     const int matrixDim = 3;
 
     A.resize( matrixDim, matrixDim, 0);
     C.resize( matrixDim, matrixDim, 1);
-
-//
-//    std::cout << "matrix A has columns: "<< A.cols() << std::endl;
-//    std::cout << "matrix A has rows: "<< A.rows() << std::endl;
-//
-//    std::cout << "matrix C has columns: "<< C.cols() << std::endl;
-//    std::cout << "matrix C has rows: "<< C.rows() << std::endl;
 
     // set A equal to identity
     for (unsigned int i = 0; i < matrixDim; ++i)
@@ -51,7 +46,6 @@ int main()
     std::cout << "Least squares A matrix: " << std::endl;
     printMatrix(A);
 
-
     // initialise the matrix C
     C[0][1] = 2;
     C[0][2] = 3;
@@ -59,22 +53,13 @@ int main()
     C[2][0] = 2;
     C[2][2] = 4;
 
-//    int count = 1;
-//    for (unsigned int i = 0; i < matrixDim; ++i)
-//        for ( unsigned int j=0; j < matrixDim; ++j) {
-//            C[i][j] = count;
-//            count++;
-//        }
-
     std::cout << "Constraint C matrix: " << std::endl;
     printMatrix(C);
-
 
     b[2] = 1;
     exactSoln[0] = 0.75;
     exactSoln[1] = 1.5;
     exactSoln[2] = -0.25;
-//    C.mv(b,d);
 
     d[0] = 3;
     d[1] = 5;
@@ -96,55 +81,29 @@ int main()
     dVec.push_back(d);
     solnVec.push_back(x);
 
-//    std::cout << bVec.size() << std::endl;
-
     // define class member of least squares
     auto leastSquaresMinimizer = Dune::Vem::LeastSquares(A,C);
 
-//    std::vector< Dune::DynamicVector< double >> bVec, dVec, solnVec;
-
-//    std::vector< Dune::DynamicVector<double>> ;
-
-//    bVec.push_back(b);
-//    dVec.push_back(d);
-//    solnVec.resize(bVec.size(),Dune::DynamicVector<double> (3,0));
-//
-//    for (unsigned int k=0; k<solnVec[0].size(); ++k)
-//        std::cout << solnVec[0][k] << std::endl;
-
-//    solnVec = leastSquaresMinimizer.leastSquaresSolver(b,d);
-
-//    std::cout << "I reached here" << std::endl;
-//    invoke least squares solver
     leastSquaresMinimizer.leastSquaresSystem(bVec, dVec, solnVec);
-
-//    Dune::DynamicVector< double > llsSolnVec = solnVec[0];
 
     for ( unsigned int k = 0; k < solnVec[0].size(); ++k )
         std::cout << solnVec[0][k] << std::endl;
 
     // compare with exact solution
-    const double tol = 1e-10;
-
     double error = 0;
 
     assert(solnVec[0].size() == exactSoln.size());
 
     // norm llsSolnVec - exactSoln;
     for (unsigned int i = 0; i < solnVec[0].size(); ++i )
-    {
         error += (solnVec[0][i]-exactSoln[i])*(solnVec[0][i]-exactSoln[i]);
-    }
 
     error = sqrt(error);
     std::cout << "error: " << error << std::endl;
 
-    // define tolerance
-    if (error < tol)
-    {
+    if (error < tol) {
         return 0;
     }
 
     return -1;
-
 }
