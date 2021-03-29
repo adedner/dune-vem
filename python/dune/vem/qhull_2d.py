e8df5ae5e531ed0d0703546d6266167531fa3afe
dedner <a.s.dedner@warwick.ac.uk>
2021-03-29 11:25:13 +0100

    testing

--- python/dune/vem/qhull_2d.py
+++ python/dune/vem/qhull_2d.py
@@ -33,17 +33,21 @@ edge = lambda a,b: concatenate(([a],[b]))
 
 def qhull2D(sample):
     def dome(sample,base):
+        print(sample)
+        print(base)
         h, t = base
         dists = dot(sample-h, dot(((0,-1),(1,0)),(t-h)))
         outer = repeat(sample, dists>0, 0)
         print(len(outer),sample,base)
         if len(outer):
             pivot = sample[argmax(dists)]
+            print("len(outer)")
             return link(dome(outer, edge(h, pivot)),
                     dome(outer, edge(pivot, t)))
         else:
             return base
     if len(sample) > 2:
+        print("len(sample)>2")
         axis = sample[:,0]
         base = take(sample, [argmin(axis), argmax(axis)], 0)
         return link(dome(sample, base), dome(sample, base[::-1]))
