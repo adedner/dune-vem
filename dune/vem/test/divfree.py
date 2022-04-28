d1aafa2e06fc16ec69fe574b1a36967267777f6c
Alice Hodson <alice-rachel.hodson@warwick.ac.uk>
2022-04-28 15:14:23 +0100

    Add parameters to div free problem with pde solve

--- dune/vem/test/divfree.py
+++ dune/vem/test/divfree.py
@@ -1,6 +1,13 @@
 from ufl import *
 import dune.ufl
 
+parameters = {"newton.linear.tolerance": 1e-8,
+              "newton.tolerance": 5e-6,
+              "newton.lineSearch": "simple",
+              "newton.linear.verbose": False,
+              "newton.verbose": True
+              }
+
 mass = dune.ufl.Constant(1, "mu")
 
 def divfree(space, exact):
@@ -12,14 +19,14 @@ def divfree(space, exact):
     b = dot( -div(grad(exact)) + exact, v) * dx
     dbc = [dune.ufl.DirichletBC(space, [0,0], i+1) for i in range(4)]
 
-    df = space.interpolate(exact,name="solution")
-    # scheme = dune.vem.vemScheme(
-    #               [a==b, *dbc], space,
-    #             #   solver=("suitesparse","umfpack"),
-    #               parameters=parameters,
-    #               gradStabilization=0,
-    #               massStabilization=mass)
-    # info = scheme.solve(target=df)
+    # df = space.interpolate(exact,name="solution")
+    scheme = dune.vem.vemScheme(
+                  [a==b, *dbc], space,
+                #   solver=("suitesparse","umfpack"),
+                  parameters=parameters,
+                  gradStabilization=0,
+                  massStabilization=mass)
+    info = scheme.solve(target=df)
 
     edf = exact-df
     err = [inner(edf,edf)]
