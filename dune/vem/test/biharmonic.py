226e04b3f088441b8d110d25f246b2d14e001f46
Alice Hodson <alice-rachel.hodson@warwick.ac.uk>
2022-04-29 16:57:02 +0100

    Create main in biharmonic for individual pipeline tests

--- dune/vem/test/biharmonic.py
+++ dune/vem/test/biharmonic.py
@@ -9,6 +9,9 @@ import ufl.algorithms
 from ufl import *
 import dune.ufl
 
+from script import runTest, checkEOC
+from interpolate import interpolate_fourthorder
+
 parameters = {"newton.linear.tolerance": 1e-12,
               "newton.linear.preconditioning.method": "jacobi",
               "penalty": 40,  # for the bbdg scheme
@@ -55,3 +58,29 @@ def biharmonic(space, exact):
             inner(grad(grad(edf)),grad(grad(edf)))]
 
     return err
+
+def runTestBiharmonic(testSpaces, order):
+    x = SpatialCoordinate(triangle)
+    exact = as_vector( [sin(2*pi*x[0])**2*sin(2*pi*x[1])**2] )
+    spaceConstructor = lambda grid, r: dune.vem.vemSpace( grid,
+                                                          order=order,
+                                                          dimRange=r,
+                                                          testSpaces=testSpaces )
+
+    expected_eoc = [order+1, order, order-1]
+    eoc_interpolation = runTest(exact, spaceConstructor, interpolate_fourthorder)
+    eoc_solve = runTest(exact, spaceConstructor, biharmonic)
+
+    return eoc_interpolation, eoc_solve, expected_eoc
+
+def main():
+      orderslist_fourthorder = [3]
+      for order in orderslist_fourthorder:
+            print("order: ", order)
+            C1NCtestSpaces = [ [0], [order-3,order-2], [order-4] ]
+            print("C1 non conforming test spaces: ", C1NCtestSpaces)
+            eoc_interpolation, eoc_solve, expected_eoc = runTestBiharmonic( C1NCtestSpaces, order )
+
+            checkEOC(eoc_interpolation, expected_eoc)
+
+main()
