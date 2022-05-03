5a1b6429965a12af5d90457fb3215041093b7acc
Alice Hodson <alice-rachel.hodson@warwick.ac.uk>
2022-05-03 12:08:50 +0100

    varying coeff added to hk tests and arbitrary dim range

--- dune/vem/test/hk.py
+++ dune/vem/test/hk.py
@@ -4,17 +4,18 @@ import dune.ufl
 from script import runTest, checkEOC, interpolate
 from interpolate import interpolate_secondorder
 
-dimR = 1
+dimR = 3
 
 def hk(space, exact):
-    massCoeff = 1
-    diffCoeff = 1
+    x = SpatialCoordinate(space)
+    massCoeff = 1+sin(dot(x,x))
+    diffCoeff = 1-0.9*cos(dot(x,x))
     assert(dimR == exact.ufl_shape[0]), "dim range not compatible"
 #     dimR = exact.ufl_shape[0]
     u = TrialFunction(space)
     v = TestFunction(space)
 
-    a = (inner(grad(u),grad(v)) + dot(u,v) ) * dx
+    a = (diffCoeff*inner(grad(u),grad(v)) + massCoeff*dot(u,v) ) * dx
     b = sum( [ ( -div(diffCoeff*grad(exact[i])) + massCoeff*exact[i] ) * v[i]
              for i in range(dimR) ] ) * dx
     dbc = [dune.ufl.DirichletBC(space, exact, i+1) for i in range(4)]
@@ -52,13 +53,29 @@ def runTesthk(testSpaces, order, vectorSpace, reduced):
 
 def main():
     # test elliptic with conforming and nonconforming second order VEM space
-    orderslist_secondorder = [2]
-    for order in orderslist_secondorder:
+    orders = [1,3]
+    for order in orders:
        print("order: ", order)
+
+       # C0 non conforming VEM
        C0NCtestSpaces = [-1,order-1,order-2]
        print("C0 non conforming test spaces: ", C0NCtestSpaces)
-       eoc_solve, expected_eoc = runTesthk( C0NCtestSpaces, order, False, False )
+       eoc, expected_eoc = runTesthk( C0NCtestSpaces, order, False, False )
 
-       checkEOC(eoc_solve, expected_eoc)
+       checkEOC(eoc, expected_eoc)
+
+       # C0 conforming VEM
+       C0testSpaces = [0,order-2,order-2]
+       print("C0 conforming test spaces: ", C0testSpaces)
+       eoc, expected_eoc = runTesthk( C0testSpaces, order, False, False )
+
+       checkEOC(eoc, expected_eoc)
+
+       # C0 serendipity VEM
+       C0serendipitytestSpaces = [0,order-2,order-3]
+       print("C0 serendipity test spaces: ", C0serendipitytestSpaces)
+       eoc, expected_eoc = runTesthk( C0serendipitytestSpaces, order, False, False )
+
+       checkEOC(eoc, expected_eoc)
 
 main()
