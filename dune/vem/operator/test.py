be823bed6515566aeab516b33cc3882a40ccabe9
A Dedner <a.s.dedner@warwick.ac.uk>
2024-09-05 14:34:21 +0100

    [fix]

--- dune/vem/operator/test.py
+++
@@ -1,9 +0,0 @@
-from dune.grid import cartesianDomain
-from dune.vem import polyGrid
-from dune.vem import vemSpace
-
-grid = polyGrid( cartesianDomain([0,0],[1,1],[10,10]) )
-spc = vemSpace(grid)
-
-S = spc.stabilization()
-print(S.as_numpy)
