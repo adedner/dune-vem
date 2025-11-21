d6a1a7e7e23363ec680d8517569ba2462a2315a6
A Dedner <a.s.dedner@warwick.ac.uk>
2025-11-21 13:56:02 +0000

    make it possible to add stabilizaton by hand to existing matrix

--- dune/vem/operator/stabmatrix.hh
+++ dune/vem/operator/stabmatrix.hh
@@ -10,37 +10,85 @@
 #include <dune/fem/function/common/localcontribution.hh>
 #include <dune/fem/common/bindguard.hh>
 #include <dune/fem/operator/common/temporarylocalmatrix.hh>
+#include <dune/fem/quadrature/cachingquadrature.hh>
 
 namespace Dune
 {
   namespace Vem
   {
-    template< class LinOperator >
-    void stabilization(LinOperator &op,
-         double hessStab, double gradStab, double massStab)
+    template< class LinOperatorS, class DSpace, class RSpace, class LinOperator>
+    void stabilization(const LinOperatorS &op,
+                       const DSpace &domainSpace, const RSpace &rangeSpace,
+                       double hessStab, double gradStab, double massStab,
+                       LinOperator &out)
     {
       typedef typename LinOperator::DomainFunctionType DomainFunctionType;
       typedef typename LinOperator::RangeFunctionType RangeFunctionType;
       typedef typename RangeFunctionType::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+      typedef typename DiscreteFunctionSpaceType::RangeType RangeType;
+      typedef typename DiscreteFunctionSpaceType::JacobianRangeType JacobianRangeType;
+      typedef typename DiscreteFunctionSpaceType::HessianRangeType HessianRangeType;
       typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
       typedef typename GridPartType::template Codim< 0 >::EntitySeedType ElementSeedType;
 
-      const DiscreteFunctionSpaceType &domainSpace = op.domainSpace();
-      const DiscreteFunctionSpaceType &rangeSpace = op.rangeSpace();
       const int domainBlockSize = domainSpace.localBlockSize;
       const GridPartType &gridPart = rangeSpace.gridPart();
       const auto &agIndexSet    = rangeSpace.blockMapper().indexSet();
       const auto &agglomeration = rangeSpace.agglomeration();
 
-      Fem::DiagonalStencil< DiscreteFunctionSpaceType, DiscreteFunctionSpaceType >
-           stencil(domainSpace,rangeSpace);
-      op.reserve( stencil );
-      op.clear();
       typedef Dune::Fem::TemporaryLocalMatrix< DiscreteFunctionSpaceType,
                                                DiscreteFunctionSpaceType > TemporaryLocalMatrixType;
       TemporaryLocalMatrixType jLocal( domainSpace, rangeSpace );
+      TemporaryLocalMatrixType diagLocal( domainSpace, rangeSpace );
+
+      ////////////////////////
+      std::vector< ElementSeedType > seeds( agglomeration.size() );
+      std::vector< std::vector<double> > diagLocal0( agglomeration.size() );
+      std::vector< std::vector<double> > diagLocal1( agglomeration.size() );
+      std::vector< std::vector<double> > diagLocal2( agglomeration.size() );
+      typedef Dune::Fem::ElementQuadrature< GridPartType, 0, Dune::FemPy::FempyQuadratureTraits > QuadratureType;
       for (const auto &entity : Dune::elements(gridPart, Dune::Partitions::interiorBorder))
       {
+        const unsigned int agglomerate = agglomeration.index(entity);
+        const auto &baseSet = domainSpace.basisFunctionSet(entity);
+        const std::size_t numDofs = baseSet.size();
+        if (!seeds[ agglomerate ].isValid())
+        {
+          seeds[ agglomerate ] = entity.seed();
+          diagLocal0[ agglomerate ].resize(numDofs,0);
+          diagLocal1[ agglomerate ].resize(numDofs,0);
+          diagLocal2[ agglomerate ].resize(numDofs,0);
+        }
+        std::vector< RangeType > phi( numDofs );
+        std::vector< JacobianRangeType > dphi( numDofs );
+        std::vector< HessianRangeType > d2phi( numDofs );
+        QuadratureType quadrature( entity, 2*domainSpace.order() );
+        const auto &geometry = entity.geometry();
+        const size_t numQuadraturePoints = quadrature.nop();
+        for( size_t pt = 0; pt < numQuadraturePoints; ++pt )
+        {
+          const typename QuadratureType::CoordinateType &x = quadrature.point( pt );
+          const double weight = quadrature.weight( pt ) * geometry.integrationElement( x );
+          baseSet.evaluateAll( quadrature[ pt ], phi );
+          baseSet.jacobianAll( quadrature[ pt ], dphi );
+          baseSet.hessianAll( quadrature[ pt ], d2phi );
+          for ( size_t i=0;i<numDofs;++i)
+          {
+            for (size_t r=0;r<RangeType::dimension;++r)
+            {
+              diagLocal0[ agglomerate ][i] += phi[i][r]*phi[i][r] * weight;
+              diagLocal1[ agglomerate ][i] += dphi[i][r]*dphi[i][r] * weight;
+              for (size_t c=0;c<d2phi[i][r].size();++c)
+                diagLocal2[ agglomerate ][i] += d2phi[i][r][c][c]*d2phi[i][r][c][c] * weight;
+            }
+          }
+        }
+      }
+      ////////////////////////
+
+      for (const auto &seed : seeds)
+      {
+        const auto entity = gridPart.entity( seed );
         const std::size_t agglomerate = agglomeration.index( entity );
         const auto &bbox = agIndexSet.boundingBox( agglomerate );
         double bbH2 = pow(bbox.volume()/bbox.diameter(),2);
@@ -50,28 +98,44 @@ namespace Dune
         std::size_t bs = domainBlockSize;
         assert( jLocal.rows()    == stabMatrix.rows()*bs );
         assert( jLocal.columns() == stabMatrix.cols()*bs );
-        assert( stabMatrix.cols()*bs == uLocal.size() );
 
-        auto stab = gradStab + massStab*bbH2 + hessStab/bbH2;
-
+        // auto stab = gradStab + massStab*bbH2 + hessStab/bbH2;
+        std::vector<double> sRow(stabMatrix.cols());
         for (std::size_t r = 0; r < stabMatrix.rows(); ++r)
+        {
+          for (std::size_t c = 0; c < stabMatrix.cols(); ++c)
+          {
+            sRow[c] = 0;
+            for (std::size_t k = 0; k < stabMatrix.cols(); ++k)
+            {
+              double fac = massStab*std::max(diagLocal0[agglomerate][k], bbH2) +
+                           gradStab*std::max(diagLocal1[agglomerate][k], gradStab) +
+                           hessStab*std::max(diagLocal2[agglomerate][k], 1/bbH2);
+              sRow[c] += stabMatrix[k][r] * stabMatrix[k][c] * fac;
+            }
+          }
           for (std::size_t c = 0; c < stabMatrix.cols(); ++c)
             for (std::size_t b = 0; b < bs; ++b)
-            {
-              /*
-              std::cout << "(" << agglomerate << "," << r*bs+b << "," << c*bs+b << "):"
-                        << "---" << " + "
-                        << stab * stabMatrix[r][c]
-                        << " stab matrix "
-                        << stabMatrix[r][c] << ","
-                        << stab
-                        << std::endl;
-              */
-              jLocal.add(r*bs+b, c*bs+b, stab*stabMatrix[r][c]);
-            }
-        op.addLocalMatrix( entity, entity, jLocal );
+              jLocal.add(r*bs+b, c*bs+b, sRow[c]);
+        }
+        out.addLocalMatrix( entity, entity, jLocal );
       }
-      op.flushAssembly();
+      out.flushAssembly();
+    }
+
+    template< class LinOperator >
+    void stabilization(LinOperator &op,
+         double hessStab, double gradStab, double massStab)
+    {
+      typedef typename LinOperator::DomainFunctionType DomainFunctionType;
+      typedef typename LinOperator::RangeFunctionType RangeFunctionType;
+      typedef typename RangeFunctionType::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+      Fem::DiagonalStencil< DiscreteFunctionSpaceType, DiscreteFunctionSpaceType >
+           stencil(op.domainSpace(),op.rangeSpace());
+      op.reserve( stencil );
+      op.clear();
+      stabilization(op,op.domainSpace(),op.rangeSpace(),
+                    hessStab,gradStab,massStab,op);
     }
 
   } // namespace Vem
